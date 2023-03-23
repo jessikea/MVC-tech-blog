94d963f1ae2ccd955d4d3ec561d19bc7d00a1858
jessikea <jessikea@jtran.git>
2023-03-22 20:43:42 -0400

    index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,62 +1,11 @@
 const router = require('express').Router();
-const { User, Comment, Post } = require('../../models');
-// const withAuth = require('../../utils/auth');
 
-// GET all post
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
-router.get('/', async (req, res) => {
-    try{
-        const postData = await Post.findAll({
-            include: [User],
-        });
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
 
-        const posts = postData.map((post) => post.get({ plain: true }));
-        res.render('User-Posts', { posts});
-
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// GET one post
-router.get('/post/:id', async (req, res) => {
-    try {   
-        const postData = await Post.findByPk(req.params.id, {
-            include: [
-            User, {
-                model: Comment, include: [User] 
-            }
-            ],
-            });
-            if (postData){
-                const post = postData.get({ plain: true });
-                res.render('User-Post', { post });
-            }
-            else {
-                res.status(404).end();
-            }
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    });
-
-    // get signup page
-    router.get('/signup', (req, res) => {
-        if (req.session.loggedIn) {
-            res.redirect('/');
-            return;
-        }
-        res.render('signup');
-    });
-
-    // get login page
-    router.get('/login', (req, res) => {
-        if (req.session.loggedIn) {
-            res.redirect('/');
-            return;
-        }
-        res.render('login');
-    }
-    );
-
-    module.exports = router;
+module.exports = router;
